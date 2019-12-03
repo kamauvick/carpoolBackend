48d5612519dc373cff401cd0c338bb4276e58b77
Steve <mittosteve@gmail.com>
2019-12-03 15:15:27 +0300

    setting all field to null so ass to allow creating of an instance through receivers

--- WBBackend/models.py
+++ WBBackend/models.py
@@ -5,16 +5,16 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=100)
-    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, default='sample.jpg')
+    first_name = models.CharField(max_length=200,null=True)
+    last_name = models.CharField(max_length=200,null=True)
+    phone_number = models.CharField(max_length=100,null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, default='sample.jpg',null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
         if created:
-            Profile.objects.create(user=instance)
+            Profile.objects.create(user=instance,first_name=instance.username)
 
     @receiver(post_save, sender=User)
     def save_profile(sender, instance, **kwargs):
