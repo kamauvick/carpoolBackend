fa010d5fa98cd3998599b3548dc315584eda71b9
kamauvick <kamauvick@outlook.com>
2019-12-04 17:32:53 +0300

    Create a profile serializer with create and update methods.

--- WBBackend/serializers.py
+++ WBBackend/serializers.py
@@ -1,2 +1,33 @@
-from .models import *
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from .models import *
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        raise ValidationError(detail='The Profile cannot be created.')
+
+    def update(self, instance, validated_data):
+        if self.context['request'].user != instance.user:
+            raise ValidationError(detail='You must be a user to edit.')
+        phone_number = validated_data.get('phone_number', None)
+        # profile_pic = validated_data.get('profile_pic', None)
+        print(phone_number)
+        if phone_number is None:
+            if not self.partial:
+                raise ValidationError(detail='The Phone_number must be provided.')
+            else:
+                instance.phone_number = phone_number
+                # instance.profile_pic = profile_pic
+                instance.save()
+            return instance
+
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'phone_number', 'profile_pic', 'user', 'device_id', ]
+        read_only_fields = ['user', 'device_id', ]
+
+
+
