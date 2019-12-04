9bea6042b72629cc873a5b3c0cab5cc93651051b
MungaiKeren <wambukeren@gmail.com>
2019-12-04 21:48:02 +0300

    This commit serializes the offers model

--- WBBackend/serializers.py
+++ WBBackend/serializers.py
@@ -1,2 +1,8 @@
 from .models import *
 from rest_framework import serializers
+
+class OfferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Offer
+        fields = ('driver','origin','destination','available_seats',
+                'departure_time','created_at', 'is_full','is_ended')
