0e7fe12d9d23ae8aa369f0a223a7c263f6dfab93
kamauvick <kamauvick@outlook.com>
2019-12-04 17:34:29 +0300

    Add an auth endpoint to the app url file.
    add a profile router to the urls.

--- WBBackend/urls.py
+++ WBBackend/urls.py
@@ -1,15 +1,25 @@
-from django.urls import path,re_path,include
-from . import views
+from django.urls import re_path, include
 
-#FCM NOTIFICATIONS
-from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet,FCMDeviceViewSet
+# FCM NOTIFICATIONS
+from fcm_django.api.rest_framework import FCMDeviceViewSet
 from rest_framework.routers import DefaultRouter
 
 # Switch to FCMDeviceAuthorizedViewSet when authentication is ready
 router = DefaultRouter()
 router.register(r'devices', FCMDeviceViewSet)
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    ProfileView
+)
+
+router = SimpleRouter()
+router.register('profile', ProfileView)
 
 urlpatterns = [
     # path('main/' ,views.home, name='home'),
-    re_path(r'^', include(router.urls))
+    re_path(r'^', include(router.urls)),
+    re_path(r'^accounts/', include('allauth.urls')),
+    re_path(r'^auth/', include('rest_auth.urls')),
+    re_path(r'^auth/register/', include('rest_auth.registration.urls')),
 ]
