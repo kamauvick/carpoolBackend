0fccd72583a6235109f3e1d263f92b1b97cabfb4
kamauvick <kamauvick@outlook.com>
2019-12-04 17:35:16 +0300

    Add a ProfileView to display user profiles.

--- WBBackend/views.py
+++ WBBackend/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Profile
+from .serializers import ProfileSerializer
+
 
 # Create your views here.
+
+
+class ProfileView(ModelViewSet):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    def get_queryset(self):
+        profile = self.request.user.profile
+        if self.action in ['list', 'retrieve']:
+            return [profile]
+
+    def get_object(self):
+        return self.request.user.profile
