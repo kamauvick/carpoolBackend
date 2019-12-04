ef5f745ecd7642454eda71aecf885e2c8eb1a408
MungaiKeren <wambukeren@gmail.com>
2019-12-04 21:48:56 +0300

    This commit creates an offers view

--- WBBackend/views.py
+++ WBBackend/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializers import *
 from . import notification
 from django.contrib.auth.models import User
+
+
+# Create your views here.
 def home(request):
     user = User.objects.get(pk = 1)
     notification.request_notifications(user,"Hey Steve","This is a test")
     return JsonResponse({'user':user.username})
-# Create your views here.
+
+
+class OffersList(APIView):
+    def get(self, request, format=None):
+        all_offers = Offer.objects.all()
+        serializers = OfferSerializer(all_offers, many=True)
+        return Response(serializers.data)
+
+
