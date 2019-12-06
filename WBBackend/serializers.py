833b5b98f471b0fad71ff67a50c014334dbf0a65
Steve <mittosteve@gmail.com>
2019-12-07 01:34:54 +0300

    serializing request board and creating an api view for accssing the data

--- WBBackend/serializers.py
+++ WBBackend/serializers.py
@@ -1,2 +1,21 @@
 from .models import *
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
+
+class RequestBoardSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        exsist = RequestBoard.objects.filter(
+            demand=validated_data['demand']).first()
+        if exsist:
+            serializer = RequestBoardSerializer(exsist)
+            details = {"status": status.HTTP_400_BAD_REQUEST,
+                       "message": "User already has an exsisting request.Cancel requet to make another",
+                       "request_board": serializer.data}
+            raise ValidationError(detail=details)
+        return RequestBoard.objects.create(**validated_data)
+
+    class Meta:
+        model = RequestBoard
+        exclude = ("")
--- WBBackend/views.py
+++ WBBackend/views.py
@@ -1,10 +1,56 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from . import notification
-from django.contrib.auth.models import User
-def home(request):
-    user = User.objects.get(pk = 1)
-    notification.request_notifications(user,"Hey Steve","This is a test")
-    return JsonResponse({'user':user.username})
-# Create your views here.
+from .serializers import *
+
+from rest_framework import mixins, permissions
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import filters
+from rest_framework.exceptions import ValidationError
+
+
+class RequestBoardViewSet(ModelViewSet):
+    queryset = RequestBoard.objects.all()
+    serializer_class = RequestBoardSerializer
+    # permission_classes =
+
+    def get_queryset(self):
+        '''
+        This function is ment to allow query params search if no query params
+        it returns all the available requests
+        '''
+        demand = self.request.query_params.get('demand')
+        status = self.request.query_params.get('status')
+        offer = self.request.query_params.get('offer')
+        if status and demand:
+            options = ["PE", "AC", "DE"]
+            status = "PE" if 'p' in status.lower() else status
+            status = "AC" if 'a' in status.lower() else status
+            status = "DE" if 'd' in status.lower() else status
+            if status not in options:
+                raise ValidationError(detail={"error": "invalid stautus"})
+            return RequestBoard.objects.filter(demand__pk=int(demand), status=status)
+        if demand:
+            return RequestBoard.objects.filter(demand__pk=int(demand))
+        if offer:
+            return RequestBoard.objects.filter(offer__pk=int(offer))
+        return RequestBoard.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        '''
+        This function is ment to allow creating of a request board
+        '''
+        profile = request.user.profile
+        return self.create(request, profile, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        '''
+        This function allows drivers to update the request status
+        '''
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        '''
+        This function is ment to allow cancling a request by deleteing a request
+        '''
+        return self.destroy(request, *args, **kwargs)
