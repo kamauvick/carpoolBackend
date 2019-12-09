d8d425a54c6a8b015962508411ec4351c7213a80
Steve <mittosteve@gmail.com>
2019-12-09 23:18:13 +0300

    adding authentication classes to settings

--- WBCarPoolApp/settings.py
+++ WBCarPoolApp/settings.py
@@ -54,7 +54,13 @@ FCM_DJANGO_SETTINGS = {
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'] ,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+     'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+      'rest_framework.permissions.IsAuthenticated'
+    ),
 }
 
 AUTHENTICATION_BACKENDS = (
