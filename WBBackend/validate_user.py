b6b445222f24d82ae57476551a51fd41332ce28e
kamauvick <kamauvick@outlook.com>
2019-12-16 11:56:43 +0300

    Add a function to generate user tokens
    STILL IN TEST MODE.

--- WBBackend/validate_user.py
+++ WBBackend/validate_user.py
@@ -1,41 +1,52 @@
-import requests 
+import requests
 import re
+from rest_framework.authtoken.models import Token
+from decouple import config
 
 
-class ValidateUser():
+class ValidateUser:
     """
     A class to `Validate user data` and `obtain a user object`
     """
-    #Validate Passed address Email
+
+    # Validate Passed address Email
     def validate_email(self, email):
         """
         A function to `validate the user emails` passed by the user. It uses `regex` to validate whether
         the email is valid or not.
         
         """
-        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-        if re.search(regex,email):
-            print('The email you provided is valid.')
+        regex = "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+        if re.search(regex, email):
+            print("The email you provided is valid.")
             return email
         else:
-            print('The email you provided is invalid.')
+            print("The email you provided is invalid.")
 
-    
-    # Check if user exists on the DB    
+    # Check if user exists on the DB
     def check_if_user_exists(self, api_key, email):
         """
         A function that get passed `query params` from the user request and `makes a get request
         to the user DB` to obtain a user object
         """
-        url= f'http://bw0rld.herokuapp.com/wb_users/?apiKey={api_key}&email={email}'
+        url = f"http://bw0rld.herokuapp.com/wb_users/?apiKey={api_key}&email={email}"
         headers = {
-            'authorization': "Token 46e32eb8c65cf1f0af7923cf2b821198ef6a8474",
-            'content-type': "application/json"
+            "authorization": config('AUTHORIZATION'),
+            "content-type": "application/json",
         }
         response = requests.request("GET", url=url, headers=headers)
         print(response.status_code)
         if response.status_code == 200:
             return response.json()
         else:
-            print(response.status_code + 'the request failed')
-         
+            print(response.status_code + "the request failed")
+
+    #Generate Random user tokens
+    def generate_token(self, username, password):
+        """
+        A function to generate User authentication tokens
+        """
+        data = {"username": username, "password": password}
+        token = Token.objects.create(user=data)
+        return token
+
