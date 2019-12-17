9bd056c80e71ff752030c0af6251035cb047492c
kamauvick <kamauvick@outlook.com>
2019-12-17 21:53:37 +0300

    Remove comments in the settings file.

--- WBCarPoolApp/settings.py
+++ WBCarPoolApp/settings.py
@@ -96,8 +96,6 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                # 'social_django.context_processors.backends',
-                # 'social_django.context_processors.login_redirect',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
