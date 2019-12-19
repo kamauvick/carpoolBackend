cb984eb2534f8dcc5f5a8d3722da8bb94f72f6c6
kamauvick <kamauvick@outlook.com>
2019-12-19 19:37:52 +0300

    Store the user token in the .env file.

--- WBBackend/validate_user.py
+++ WBBackend/validate_user.py
@@ -10,13 +10,12 @@ class ValidateUser:
     """
 
     # Validate Passed address Email
-    def validate_email(self, email):
+    def validate_email(email):
         """
         A function to `validate the user emails` passed by the user. It uses `regex` to validate whether
         the email is valid or not.
-        
         """
-        regex = "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+        regex = "^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"     
         if re.search(regex, email):
             print("The email you provided is valid.")
             return email
@@ -24,14 +23,14 @@ class ValidateUser:
             print("The email you provided is invalid.")
 
     # Check if user exists on the DB
-    def check_if_user_exists(self, api_key, email):
+    def check_if_user_exists(api_key, email):
         """
         A function that get passed `query params` from the user request and `makes a get request
         to the user DB` to obtain a user object
         """
         url = f"http://bw0rld.herokuapp.com/wb_users/?apiKey={api_key}&email={email}"
         headers = {
-            "authorization": 'Token 46e32eb8c65cf1f0af7923cf2b821198ef6a8474',
+            "authorization": config('AUTHORIZATION'),
             "content-type": "application/json",
         }
         response = requests.request("GET", url=url, headers=headers)
