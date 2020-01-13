56ae50b3fa664d3717f08f956ef6a94b6fa230bd
kamauvick <kamauvick@outlook.com>
2020-01-13 15:10:51 +0300

    Add CORS settings to the app settings.

--- WBCarPoolApp/settings.py
+++ WBCarPoolApp/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'coreapi',
     'fcm_django',
     'drf_yasg',
+    'corsheaders',
 ]
 FCM_DJANGO_SETTINGS = {
     "FCM_SERVER_KEY": config('FCM_SERVER_KEY')
@@ -87,7 +88,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'WBCarPoolApp.urls'
 
