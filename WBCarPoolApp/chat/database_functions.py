d89eee1811cb157332a5a18cab52fa362bcdaa11
Madison Ell <mellmell2550@gmail.com>
2023-01-14 13:53:05 -0800

    Create database functions

---
+++ WBCarPoolApp/chat/database_functions.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import ./components/form.js
+
+tripdetails = ["Photofile", "Team Leader", "Date", "Time", "Expected Out Time", "Call Out Time", "No. of Trip Members", "Return Time", "Trip Status", "Additional Info"]
+
+# write new trip data into table
+def write_new():
+  df = pd.read_csv('csvfilepath')
+
+  # how to store info fromtheform into somearray
+
+  # second dataframe for new row
+  df_new = pd.DataFrame(data=somearray, columns=tripdetails)
+
+  # add new row to original data
+  df.append(df_new)
+
+  # update csv file
+  trips_df.to_csv
+
+  return
+
+# read row at index
+def read(index):
+  df = pd.read_csv('csvfilepath')
+
+  # store in multiple variables?
+  # photofile = df.loc[index, 'Photofile']
+  # ...
+
+  # store in one dataframe
+  df_read = pd.DataFrame(data=df.loc([index], columns=tripdetails)
